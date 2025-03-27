affd73aca900dcdf3c5db8d1afe89f3fc592517b
mwilmes <maximilian.wilmes@hdi.de>
2025-03-27 12:29:45 +0100

    FRONTEND & BACKEND: Feature add oauth login and logout to front- and backend
    BACKEND:
    - add AuthController with /me Endpoints that return "login" attribute as string
    - add SecurityConfig for login/logout and set anyRequest to authenticated
    - add spring security dependency to pom.xml
    - add security properties to application.properties
    
    FRONTEND:
    - Add AllActionsView.tsx page
    - add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import Home from "./pages/Home.tsx";
 import {Route, Routes} from "react-router-dom";
 import CheckupActionDetails from "./pages/CheckupActionDetails.tsx";
+import AllActionsView from "./pages/AllActionsView.tsx";
 
 export default function App() {
     return (
         <>
             <Routes>
                 <Route path={"/"} element={<Home/>}></Route>
+                <Route path={"/checkup-actions"} element={<AllActionsView/>}></Route>
                 <Route path={"/checkup-actions/:id"} element={<CheckupActionDetails/>}/>
             </Routes>
         </>
---
+++ frontend/src/pages/AllActionsView.tsx
@@ -0,0 +1,39 @@
+import {useEffect, useState} from 'react';
+import CheckupActionCard from "../components/CheckupActionCard.tsx";
+import NewCheckupActionCard from "../components/NewCheckupActionCard.tsx";
+import {CheckupAction} from "../models/CheckupAction.ts";
+import axios from "axios";
+import {NavLink} from "react-router-dom";
+
+function AllActionsView() {
+
+    const [actions, setActions] = useState<CheckupAction[]>()
+
+    useEffect(() => {
+            fetchActions()
+        }, []
+    )
+
+    const fetchActions = () => {
+        axios.get("api/checkup-actions")
+            .then(r => setActions(r.data))
+            .catch(e => console.log("Error fetching actions", e))
+    }
+
+    if (!actions) {
+        return "Loading..."
+    }
+    return (
+        <>
+            {
+                actions.map((action) => (
+                    <CheckupActionCard key={action.id} action={action} fetchActions={fetchActions}/>
+                ))
+            }
+            <NewCheckupActionCard fetchActions={fetchActions}/>
+            <NavLink to={"/"}>Home</NavLink>
+        </>
+    );
+}
+
+export default AllActionsView;
