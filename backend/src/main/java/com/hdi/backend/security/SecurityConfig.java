49ae7b8873ed0b57f9f7d6c6cf69e06e3343075a
mwilmes <maximilian.wilmes@hdi.de>
2025-03-27 14:21:20 +0100

    FRONTEND & BACKEND: Feature add oauth login and logout to front- and backend
    - fix SonarQube findings
    - change title of app to Checkup Actions App

--- backend/src/main/java/com/hdi/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/hdi/backend/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
@@ -20,7 +21,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-//                .csrf(AbstractHttpConfigurer::disable) mal flo fragen, warum man das braucht (SonarQube)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(a -> a
                         .requestMatchers("/api/auth/me").authenticated()
                         .anyRequest().authenticated()
