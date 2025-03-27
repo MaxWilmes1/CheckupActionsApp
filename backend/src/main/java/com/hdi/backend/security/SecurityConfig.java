677c54d26addc65f7bba561705dbbce251742d0e
mwilmes <maximilian.wilmes@hdi.de>
2025-03-27 14:07:50 +0100

    FRONTEND & BACKEND: Feature add oauth login and logout to front- and backend
    - fix SonarQube findings

--- backend/src/main/java/com/hdi/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/hdi/backend/security/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
@@ -21,7 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable) mal flo fragen, warum man das braucht (SonarQube)
                 .authorizeHttpRequests(a -> a
                         .requestMatchers("/api/auth/me").authenticated()
                         .anyRequest().authenticated()
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,8 +2,6 @@ import Header from "../components/Header.tsx";
 
 export default function Home() {
     return (
-        <>
             <Header/>
-        </>
     )
 }
