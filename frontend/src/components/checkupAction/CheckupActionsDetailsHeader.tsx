044fdc8298497ab59be50a1c94d3e152d54dee8f
mwilmes <maximilian.wilmes@hdi.de>
2025-04-09 14:11:46 +0200

    BACKEND & FRONTEND: change data structure
    FRONTEND:
    - adjust formatting

--- frontend/src/components/checkupAction/CheckupActionsDetailsHeader.tsx
+++ frontend/src/components/checkupAction/CheckupActionsDetailsHeader.tsx
@@ -20,24 +20,23 @@ export default function CheckupActionsDetailsHeader(props: Props) {
                     display: "flex",
                     flexDirection: "column",
                 }}>
-                    <Typography variant="h6" component="h1" color="textPrimary" sx={{marginBottom: 0.5, fontWeight:"bold"}}>
+                    <Typography variant="h6" component="h1" color="textPrimary"
+                                sx={{marginBottom: 0.5, fontWeight: "bold"}}>
                         Edit Checkup Action
                     </Typography>
                     <Typography variant="caption" component="h1" color="textPrimary">
                         Action ID: {props.action.id}
                     </Typography>
                 </Box>
-                <Box sx={
-                    {
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "flex-end",
-                    }
-                }>
+                <Box sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "flex-end",
+                }}>
                     <Typography variant="body2" component="p" color="textSecondary">
                         Created: {dayjs(props.action.dateCreated).format("DD.MM.YYYY HH:mm")}
                     </Typography>
-                    <Divider/>
+                    <Divider sx={{width: "100%"}}/>
                     <Typography variant="body2" component="p" color="textSecondary">
                         Last Edit: {dayjs(props.action.dateCreated).format("DD.MM.YYYY HH:mm")}
                     </Typography>
